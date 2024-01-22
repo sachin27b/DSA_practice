5ba2aa7ffbba1638811d316e0f4dc3001a88f71a
sachin <sachin270895@gmail.com>
2024-01-23 00:41:47 +0530

    done

--- Graphs/Graphs I/Course Schedule.cpp
+++ Graphs/Graphs I/Course Schedule.cpp
@@ -1,3 +1,55 @@
+//https://leetcode.com/problems/course-schedule/
+//Using DFS topological Sort
+class Solution {
+public:
+    bool dfs(int node, int recursionCalls[], vector<int> &vis, vector<int> adj[])
+    {
+        vis[node] = 1;
+        recursionCalls[node] = 1;
+        for(auto it: adj[node])
+        {
+            if(vis[it] == 0)
+            {
+                if(dfs(it, recursionCalls, vis, adj) == false)
+                {
+                    return false;
+                }
+            }
+            else if(vis[it] == 1 && recursionCalls[it] == 1)
+            {
+                return false;
+            }
+        }
+        recursionCalls[node] = 0;
+        return true;
+
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<int> adj[numCourses];
+        for(int i = 0; i < prerequisites.size(); i++)
+        {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        // return true;
+
+        int recursionCalls[numCourses];
+        vector<int> vis(numCourses, 0);
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(vis[i] == 0)
+            {
+                if(dfs(i, recursionCalls, vis, adj) == false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+// Using Kahn's Algorithm BFS topological Sort
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
