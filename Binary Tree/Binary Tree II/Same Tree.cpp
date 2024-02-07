eea1a47e6db98c85f08f321f4b527d055a7ef21f
sachin <sachin270895@gmail.com>
2024-02-08 01:44:40 +0530

    done

---
+++ Binary Tree/Binary Tree II/Same Tree.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/same-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL)
+            return true;
+        else if((p == NULL && q != NULL) || (p != NULL && q == NULL))
+            return false;
+        if(p -> val == q -> val)
+        {
+            if(!isSameTree(p -> left, q -> left))
+            {
+                return false;
+            }
+            if(!isSameTree(p -> right, q -> right))
+            {
+                return false;
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
